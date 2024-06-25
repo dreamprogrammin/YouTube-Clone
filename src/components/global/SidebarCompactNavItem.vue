3debcf3bfd19d2fc0ad3121b45ecb2f296a986d5
test <test@MBP-Malik.lan>
2024-06-25 17:49:51 +0500

    update Sidebar

---
+++ src/components/global/SidebarCompactNavItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+import BasicIcon from '@/components/global/BasicIcon.vue';
+import { computed } from 'vue';
+
+const { name, label, isActive } = defineProps({
+  isActive: Boolean,
+  name: String,
+  label: String
+});
+
+const classes = computed(() => {
+  const classes = ['flex', 'flex-col', 'items-center', 'px-2', 'py-5', 'hover:bg-gray-300'];
+
+  return isActive ? [...classes, 'text-red-500'] : classes;
+});
+</script>
+
+<template>
+  <li>
+    <a href="#" :class="classes">
+      <basic-icon :name="name" />
+      <span class="text-xs">{{ label }}</span>
+    </a>
+  </li>
+</template>
+
+<style scoped></style>
